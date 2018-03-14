d47350648aa011661ec2df4ee969f08b42677df7
Moises Macero <mechero@users.noreply.github.com>
2018-03-14 07:29:57 +0100

    Added some basic logic to client

--- eureka-client/src/main/java/com/thepracticaldeveloper/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/thepracticaldeveloper/eurekaclient/EurekaClientApplication.java
@@ -2,6 +2,8 @@ package com.thepracticaldeveloper.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EurekaClientApplication {
@@ -9,4 +11,9 @@ public class EurekaClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
+
+	@Bean
+  public RestTemplate restTemplate() {
+	  return new RestTemplate();
+  }
 }
---
+++ eureka-client/src/main/java/com/thepracticaldeveloper/eurekaclient/controller/HolaController.java
@@ -0,0 +1,33 @@
+package com.thepracticaldeveloper.eurekaclient.controller;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Profile("spanish")
+public class HolaController {
+
+  private final RestTemplate restTemplate;
+  private final EurekaClient discoveryClient;
+  private final String englishVirtualAlias;
+
+  public HolaController(final RestTemplate restTemplate,
+                        final EurekaClient discoveryClient,
+                        @Value("${tpd.appconfig.english-alias}") final String englishVirtualAlias) {
+    this.restTemplate = restTemplate;
+    this.discoveryClient = discoveryClient;
+    this.englishVirtualAlias = englishVirtualAlias;
+  }
+
+  @GetMapping("/hola")
+  public String hola() {
+    final InstanceInfo instance = discoveryClient.getNextServerFromEureka(englishVirtualAlias, false);
+    final String theOtherResponse = restTemplate.getForObject(instance.getHomePageUrl() + "/hello", String.class);
+    return "Hola desde el cliente en Español! My English peer says: " + theOtherResponse;
+  }
+}
